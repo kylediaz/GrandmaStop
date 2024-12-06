3ed2ae82f63b15ab3c409ba27cadcc3a703fbc6f
Kyle Diaz <dev@kylediaz.com>
2024-12-06 13:16:00 -0500

    backend sms.py

--- backend/sms.py
+++ backend/sms.py
@@ -1,2 +1,12 @@
-def send_sms(message):
-    pass
+from twilio.rest import Client
+
+def send_sms(message, from_number, to_number):
+    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_=from_number,
+        body=message,
+        to=to_number
+    )
